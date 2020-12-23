520f5ad7e887b35be1522882f7aa68e1e09f9884
MaxwellTav <72995619+MaxwellTav@users.noreply.github.com>
2020-12-23 03:10:28 -0400

    Haciendo las cosas mas faciles
    Este commit optimizo un poquito mas el codigo y la manera de trabajar, evitando asi que se repita codigo, haya codigo espaguetti y/o codigo repetido e innecesario, haciendo que se obtengan los componentes y aplicando de manera general el script, dando la posibilidad de copiar y pegar el reloj y sin necesidad de programarlo ya sea funcional. [Aun trabajo en el diseño =) ]

--- TFG-Clock/Form1.Designer.cs
+++ TFG-Clock/Form1.Designer.cs
@@ -31,21 +31,23 @@
             this.components = new System.ComponentModel.Container();
             this.hoursText = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.exitButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.VoidUpdate = new System.Windows.Forms.Timer(this.components);
             this.detailTick = new System.Windows.Forms.Timer(this.components);
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.detail1 = new System.Windows.Forms.Label();
-            this.minutesText = new System.Windows.Forms.Label();
-            this.detail2 = new System.Windows.Forms.Label();
-            this.AMPM = new System.Windows.Forms.Label();
             this.secondsText = new System.Windows.Forms.Label();
+            this.AMPM = new System.Windows.Forms.Label();
+            this.detail2 = new System.Windows.Forms.Label();
+            this.minutesText = new System.Windows.Forms.Label();
+            this.detail1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
-            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // hoursText
@@ -53,10 +55,11 @@
             this.hoursText.Cursor = System.Windows.Forms.Cursors.Help;
             this.hoursText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.hoursText.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hoursText.Location = new System.Drawing.Point(-7, 12);
+            this.hoursText.Location = new System.Drawing.Point(53, 67);
             this.hoursText.Name = "hoursText";
             this.hoursText.Size = new System.Drawing.Size(59, 48);
             this.hoursText.TabIndex = 0;
+            this.hoursText.Tag = "hours";
             this.hoursText.Text = "00";
             this.hoursText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -68,40 +71,25 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(581, 21);
+            this.panel1.Size = new System.Drawing.Size(598, 21);
             this.panel1.TabIndex = 1;
             // 
-            // panel2
+            // button1
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 21);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(10, 290);
-            this.panel2.TabIndex = 2;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(10, 301);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(571, 10);
-            this.panel3.TabIndex = 2;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel4.Location = new System.Drawing.Point(571, 21);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(10, 280);
-            this.panel4.TabIndex = 2;
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(555, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 22);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "■";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // exitButton
             // 
             this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.exitButton.Location = new System.Drawing.Point(559, -1);
+            this.exitButton.Location = new System.Drawing.Point(576, -1);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(22, 22);
             this.exitButton.TabIndex = 3;
@@ -110,17 +98,32 @@
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
-            // button1
+            // panel2
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(538, -1);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(22, 22);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "■";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 410);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(10, 421);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(588, 10);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(588, 21);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 400);
+            this.panel4.TabIndex = 2;
             // 
             // VoidUpdate
             // 
@@ -131,72 +134,8 @@
             // detailTick
             // 
             this.detailTick.Enabled = true;
-            this.detailTick.Interval = 376;
+            this.detailTick.Interval = 271;
             this.detailTick.Tick += new System.EventHandler(this.detailTick_Tick);
-            // 
-            // panel5
-            // 
-            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel5.Controls.Add(this.secondsText);
-            this.panel5.Controls.Add(this.AMPM);
-            this.panel5.Controls.Add(this.detail2);
-            this.panel5.Controls.Add(this.minutesText);
-            this.panel5.Controls.Add(this.detail1);
-            this.panel5.Controls.Add(this.hoursText);
-            this.panel5.Location = new System.Drawing.Point(16, 27);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(232, 73);
-            this.panel5.TabIndex = 3;
-            // 
-            // detail1
-            // 
-            this.detail1.Cursor = System.Windows.Forms.Cursors.Help;
-            this.detail1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.detail1.Font = new System.Drawing.Font("Lucida Fax", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.detail1.Location = new System.Drawing.Point(38, 9);
-            this.detail1.Name = "detail1";
-            this.detail1.Size = new System.Drawing.Size(23, 48);
-            this.detail1.TabIndex = 1;
-            this.detail1.Text = ":";
-            this.detail1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // minutesText
-            // 
-            this.minutesText.Cursor = System.Windows.Forms.Cursors.Help;
-            this.minutesText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.minutesText.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.minutesText.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.minutesText.Location = new System.Drawing.Point(52, 12);
-            this.minutesText.Name = "minutesText";
-            this.minutesText.Size = new System.Drawing.Size(59, 48);
-            this.minutesText.TabIndex = 2;
-            this.minutesText.Text = "00";
-            this.minutesText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // detail2
-            // 
-            this.detail2.Cursor = System.Windows.Forms.Cursors.Help;
-            this.detail2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.detail2.Font = new System.Drawing.Font("Lucida Fax", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.detail2.Location = new System.Drawing.Point(93, 9);
-            this.detail2.Name = "detail2";
-            this.detail2.Size = new System.Drawing.Size(23, 48);
-            this.detail2.TabIndex = 3;
-            this.detail2.Text = ":";
-            this.detail2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // AMPM
-            // 
-            this.AMPM.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.AMPM.Cursor = System.Windows.Forms.Cursors.Help;
-            this.AMPM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.AMPM.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AMPM.Location = new System.Drawing.Point(163, 12);
-            this.AMPM.Name = "AMPM";
-            this.AMPM.Size = new System.Drawing.Size(59, 48);
-            this.AMPM.TabIndex = 6;
-            this.AMPM.Text = "XM";
-            this.AMPM.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // secondsText
             // 
@@ -204,21 +143,117 @@
             this.secondsText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.secondsText.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.secondsText.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.secondsText.Location = new System.Drawing.Point(107, 12);
+            this.secondsText.Location = new System.Drawing.Point(167, 67);
             this.secondsText.Name = "secondsText";
             this.secondsText.Size = new System.Drawing.Size(59, 48);
             this.secondsText.TabIndex = 7;
+            this.secondsText.Tag = "seconds";
             this.secondsText.Text = "00";
             this.secondsText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // AMPM
+            // 
+            this.AMPM.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.AMPM.Cursor = System.Windows.Forms.Cursors.Help;
+            this.AMPM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AMPM.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AMPM.Location = new System.Drawing.Point(223, 67);
+            this.AMPM.Name = "AMPM";
+            this.AMPM.Size = new System.Drawing.Size(59, 48);
+            this.AMPM.TabIndex = 6;
+            this.AMPM.Tag = "AMPM";
+            this.AMPM.Text = "XM";
+            this.AMPM.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // detail2
+            // 
+            this.detail2.Cursor = System.Windows.Forms.Cursors.Help;
+            this.detail2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.detail2.Font = new System.Drawing.Font("Lucida Fax", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detail2.Location = new System.Drawing.Point(153, 64);
+            this.detail2.Name = "detail2";
+            this.detail2.Size = new System.Drawing.Size(23, 48);
+            this.detail2.TabIndex = 3;
+            this.detail2.Tag = "details";
+            this.detail2.Text = ":";
+            this.detail2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minutesText
+            // 
+            this.minutesText.Cursor = System.Windows.Forms.Cursors.Help;
+            this.minutesText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minutesText.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesText.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.minutesText.Location = new System.Drawing.Point(112, 67);
+            this.minutesText.Name = "minutesText";
+            this.minutesText.Size = new System.Drawing.Size(59, 48);
+            this.minutesText.TabIndex = 2;
+            this.minutesText.Tag = "minutes";
+            this.minutesText.Text = "00";
+            this.minutesText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // detail1
+            // 
+            this.detail1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.detail1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.detail1.Font = new System.Drawing.Font("Lucida Fax", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detail1.Location = new System.Drawing.Point(98, 64);
+            this.detail1.Name = "detail1";
+            this.detail1.Size = new System.Drawing.Size(23, 48);
+            this.detail1.TabIndex = 1;
+            this.detail1.Tag = "details";
+            this.detail1.Text = ":";
+            this.detail1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DarkRed;
+            this.panel5.Location = new System.Drawing.Point(50, 51);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(250, 10);
+            this.panel5.TabIndex = 8;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.DarkRed;
+            this.panel6.Location = new System.Drawing.Point(50, 118);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(250, 10);
+            this.panel6.TabIndex = 9;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.DarkRed;
+            this.panel7.Location = new System.Drawing.Point(300, 51);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(10, 77);
+            this.panel7.TabIndex = 9;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.DarkRed;
+            this.panel8.Location = new System.Drawing.Point(40, 51);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(10, 77);
+            this.panel8.TabIndex = 10;
             // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 311);
+            this.ClientSize = new System.Drawing.Size(598, 431);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);
+            this.Controls.Add(this.secondsText);
+            this.Controls.Add(this.AMPM);
+            this.Controls.Add(this.detail2);
+            this.Controls.Add(this.minutesText);
             this.Controls.Add(this.panel4);
+            this.Controls.Add(this.detail1);
             this.Controls.Add(this.panel3);
+            this.Controls.Add(this.hoursText);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -227,7 +262,6 @@
             this.Text = "Reloj";
             this.Load += new System.EventHandler(this.Index_Load);
             this.panel1.ResumeLayout(false);
-            this.panel5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -243,12 +277,15 @@
         private System.Windows.Forms.Panel panel4;
         public System.Windows.Forms.Timer VoidUpdate;
         public System.Windows.Forms.Timer detailTick;
-        private System.Windows.Forms.Panel panel5;
         public System.Windows.Forms.Label AMPM;
         public System.Windows.Forms.Label detail2;
         public System.Windows.Forms.Label minutesText;
         public System.Windows.Forms.Label detail1;
         public System.Windows.Forms.Label secondsText;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
     }
 }
 
--- TFG-Clock/Form1.cs
+++ TFG-Clock/Form1.cs
@@ -1,82 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TFG_Clock
 {
     public partial class Index : Form
     {
+
         public Index()
         {
             InitializeComponent();
         }
-
-        //Ticki Event (Reloj / Timer)
-        private void clock_Tick(object sender, EventArgs e)
-        {
-            hoursText.Text = DateTime.Now.ToString("hh");
-            minutesText.Text = DateTime.Now.ToString("mm");
-            secondsText.Text = DateTime.Now.ToString("ss");
-            AMPM.Text = DateTime.Now.ToString("tt");
-        }
-
-        private void detailTick_Tick(object sender, EventArgs e)
-        {
-            string variante = hoursText.Text;
-            //que parpadeen los separadores
-            if(detail1.Visible)
-            {
-                detail1.Visible = false;
-            }
-            else
-            {
-                detail1.Visible = true;
-            }
-
-            //que parpadeen los separadores
-            if (detail2.Visible)
-            {
-                detail2.Visible = false;
-            }
-            else
-            {
-                detail2.Visible = true;
-            }
-        }
-
-        //Al iniciar
+        
+        //void Start() | Al iniciar
         private void Index_Load(object sender, EventArgs e)
         {
             //Obteniendo los componentes y seteando todo
             GetComponent();
         }
-
-        public void GetComponent()
+        
+        //Esta funcion se llama al iniciar el programa para setear todo
+        private void GetComponent()
         {
             //Getting Component and Setting All
             VoidUpdate.Enabled = true;
             VoidUpdate.Interval = 1000;
         }
+        
+        
 
+
+        //Ticks--------------------------------------------------------
+
+        //Ticki Event (Reloj / Timer)
+        private void clock_Tick(object sender, EventArgs e)
+        {
+            //Reloj digital, individual y flexible
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl.Tag == "minutes")
+                {
+                    ctrl.Text = DateTime.Now.ToString("mm");
+                }
+                if (ctrl.Tag == "seconds")
+                {
+                    ctrl.Text = DateTime.Now.ToString("ss");
+                }
+                if (ctrl.Tag == "hours")
+                {
+                    ctrl.Text = DateTime.Now.ToString("hh");
+                }
+                if (ctrl.Tag == "AMPM")
+                {
+                    ctrl.Text = DateTime.Now.ToString("tt");
+                }
+            }
+        }
+        
+        //que parpadeen los separadores
+        private void detailTick_Tick(object sender, EventArgs e)
+        {
+            foreach (Control detailsCtrl in Controls)
+            {
+                if (detailsCtrl.Tag == "details")
+                {
+                    if (detailsCtrl.Visible)
+                    {
+                        detailsCtrl.Visible = false;
+                    }
+                    else
+                    {
+                        detailsCtrl.Visible = true;
+                    }
+                }
+            }
+        }
+
+
+
+
+        //-------------------------------------------------Codigo "Botones"
+
+        //                  boton de minimizar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        //                  boton Salir
         private void exitButton_Click(object sender, EventArgs e)
         {
+            //preguntando si quiere salir
             DialogResult = MessageBox.Show("¿Está seguro que desea salir de la aplicación?", "Está saliendo de la app!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
             if (DialogResult == DialogResult.Yes)
             {
                 Application.Exit();
             }
-        }
-
-        //boton de minimizar
-        private void button1_Click(object sender, EventArgs e)
-        {
-            WindowState = FormWindowState.Minimized;
         }
     }
 }
